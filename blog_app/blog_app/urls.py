9435751d23e0eb3b0e67ca357bd580ebb1d51d0d
Uri Baynesay <uri552@gmail.com>
2024-05-27 16:11:18 +0300

    home and about page styling

--- blog_app/blog_app/urls.py
+++ blog_app/blog_app/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog import urls
+from blog import urls as blog_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include(urls)),
+    path('', include(blog_urls)),
 ]
