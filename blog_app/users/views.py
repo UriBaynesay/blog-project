b32bb06d2547ab835a4e60fb09dcedad943774f7
Uri Baynesay <uri552@gmail.com>
2024-05-30 11:05:56 +0300

    added login logout

--- blog_app/users/views.py
+++ blog_app/users/views.py
@@ -8,10 +8,9 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            print(form.cleaned_data)
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account successfully created for {username} !')
-            return redirect('blog-home')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
